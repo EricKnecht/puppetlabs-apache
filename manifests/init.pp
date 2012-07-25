bf3612087ca88c61190749b6fda2662b419a9702
Eric Knecht <eric@ericknecht.com>
2012-07-25 12:05:53 -0400

    remove purge option, it kills the php module

--- manifests/init.pp
+++ manifests/init.pp
@@ -31,7 +31,7 @@ class apache {
     ensure  => directory,
     path    => $apache::params::vdir,
     recurse => true,
-    purge   => true,
+    purge   => false,
     notify  => Service['httpd'],
     require => Package['httpd'],
   }
